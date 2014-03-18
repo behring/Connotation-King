8a138c25d8c2dba44d72202854b0772349264fd9
behring'mac <zhaolin0801@gmail.com>
2014-03-18 21:55:55 +0800

    增加订阅提示和输入信息反馈提示

--- controller/index_controller.rb
+++ controller/index_controller.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
+require 'wei-backend'
+token "connotation-king"
 
 get '/index' do
 	"hello sinatra"
+end
+
+on_text do
+    "你发送了如下内容:  #{params[:Content]}"
+end
+
+on_subscribe do
+    "感谢您订阅“内含王”，希望每天为您带来开心！"
 end
