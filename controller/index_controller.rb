d0f6cd6f6c390b7047e621cd525ba2f35144f76c
behring'mac <zhaolin0801@gmail.com>
2014-03-19 10:29:26 +0800

    发送1返回图文信息

--- controller/index_controller.rb
+++ controller/index_controller.rb
@@ -7,6 +7,23 @@ get '/index' do
 	"hello sinatra"
 end
 
+def pictrue_text_info
+  on_text do
+    [{
+         :title => '收到一个文本消息，返回两个图文消息',
+         :description => 'desc',
+         :picture_url => 'pic url',
+         :url => 'url'
+     },
+     {
+         :title => '这是第二个图文消息',
+         :description => 'desc1',
+         :picture_url => 'pic url1',
+         :url => 'url1'
+     }]
+  end
+end
+
 on_text do
   user_input_content = params[:Content].strip
   case user_input_content
@@ -16,7 +33,7 @@ on_text do
       2 幽默笑话
       3 心灵物语"
     when "1"
-      "你发送了如下内容:  #{user_input_content}"
+      pictrue_text_info
     when "2"
       "你发送了如下内容:  #{user_input_content}"
     when "3"
@@ -27,20 +44,6 @@ on_text do
 
 end
 
-# on_text do
-#   [{
-#        :title => '收到一个文本消息，返回两个图文消息',
-#        :description => 'desc',
-#        :picture_url => 'pic url',
-#        :url => 'url'
-#    },
-#    {
-#        :title => '这是第二个图文消息',
-#        :description => 'desc1',
-#        :picture_url => 'pic url1',
-#        :url => 'url1'
-#    }]
-# end
 
 on_subscribe do
     "感谢您订阅“内含王”\n
