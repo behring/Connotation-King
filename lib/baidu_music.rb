0e0e6109c3b5e95a6c1091ed220c76043a5bbc66
behring'mac <zhaolin0801@gmail.com>
2014-03-25 13:53:07 +0800

    test music mode

--- lib/baidu_music.rb
+++ lib/baidu_music.rb
@@ -12,6 +12,9 @@ class BaiduMusic
 
 	private
 	def self.request_baidu_inteface(singer,song)
+p "-------------------------------#{singer}   #{song}----------------------------------"
+			
+
 		url = "http://box.zhangmen.baidu.com/x?op=12&count=1&title=#{song}$$#{singer}$$$$"
 		page = Nokogiri::XML(open(URI.encode(url)))
 		count = page.xpath('/result/count').text.to_i
